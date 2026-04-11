439d11d007f135defd9304946bcb6c8f4d6cea59
Csaba-Zsolt Pápai <csaba.papai@stud.ubbcluj.ro>
2026-04-11 21:17:35 +0300

    Increase initial user simulation count to 100000

--- src/data_generator.py
+++ src/data_generator.py
@@ -5,6 +5,6 @@ from src.utils.utils import OUTPUT_DIR
 
 
 if __name__ == "__main__":
-    simulate(seed=1, N_initial=10000, save_snapshots=True)
+    simulate(seed=1, N_initial=100000, save_snapshots=True)
     uploaded_files = upload_snapshots(OUTPUT_DIR)
     print(f"ADLS upload done. Uploaded files: {uploaded_files}", flush=True)
